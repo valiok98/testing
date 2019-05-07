716c1ca144d972a43daedf313a3d0f1a946b9efd
Valio98 <valentin1998v@gmail.com>
2019-05-07 11:08:18 +0200

    Initial Commit

---
+++ OpenCV/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenCV
+//
+//  Created by Valentin Kostadinov on 07.05.19.
+//  Copyright © 2019 Valentin Kostadinov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
