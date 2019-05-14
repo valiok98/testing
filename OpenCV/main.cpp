8b8de27e13f7e71b5e319ff6c690e4f8734e8cbd
Valio98 <valentinkostadinov@Valentins-MacBook-Pro.local>
2019-05-14 09:42:22 +0200

    Shape detection.

--- OpenCV/main.cpp
+++ OpenCV/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <opencv2/opencv.hpp>
 #include <opencv2/core.hpp>
 #include <opencv2/videoio.hpp>
@@ -11,7 +12,11 @@
 
 #define MODE 0 // This is the mode of thresholding. Currently supported : 0 | 1
 
+cv::RNG rng(12345);
+
 int main() {
+    std::vector<std::vector<cv::Point>> contours;
+    std::vector<cv::Vec4i> hierarchy;
     Window* threshold_window = new Window(MODE ? "Adaptive thresholding" : "Normal thresholding"),
         * trackbar_window = new Window("Trackbar window");
     
@@ -42,19 +47,29 @@ int main() {
         
         cap >> threshold_frame;
         cap >> threshold_frame_copy;
+        
         try {
             cv::cvtColor(threshold_frame, threshold_frame, CV_BGR2GRAY);
             // Set frame.
             threshold_fr_hp->setFrame(threshold_frame);
             // Apply thresholding.
             threshold_fr_hp->thresholding();
+            // Find contours.
+            cv::findContours(threshold_frame, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0));
+            // Draw contours.
+            for( int i = 0; i < contours.size(); i++ ) {
+                    contours[i] = cv::boundingRect(contours[i]);
+                    cv::approxPolyDP(contours[i], contours[i], cv::arcLength(contours[i], true) * 0.02, true);
+                    cv::drawContours(threshold_frame, contours, i, CV_BGR2HLS, 2, 8, hierarchy);
+            }
         } catch(cv::Exception& err) {
             // Let it slide, happens on first try. Continue with the safe copy instead.
             threshold_frame = threshold_frame_copy;
         }
+        
         // Show the thresholded image.
         cv::imshow(threshold_window->getName(), threshold_frame);
-        
+    
         if (cv::waitKey(10) == 27) {
             break;
         }
